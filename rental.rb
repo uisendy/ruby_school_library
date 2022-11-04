4afb401f51e4771f89c98ce56566c5f39835e4c4
Inieke Sendy <ini.e.sendy@gmail.com>
2022-11-04 12:19:36 +0100

    [association]: Add: rental class

---
+++ rental.rb
@@ -0,0 +1,15 @@
+class Rental
+  attr_accessor :date
+  attr_reader :person, :book
+
+  def initialize(date, person, book)
+    @date = date
+
+    @person = person
+    person.rental << self
+
+    @book = book
+    book.rental << self
+  end
+
+end
