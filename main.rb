2d3bb4c3adc3781d90d38958b5784e22b82eb29b
Inieke Sendy <ini.e.sendy@gmail.com>
2022-11-05 14:37:51 +0100

    [UI]: Add: Methods: Take input and options _

--- main.rb
+++ main.rb
@@ -21,7 +21,37 @@ def display_options
   puts '7. - Exit'
 end
 
+def take_input
+  print 'Enter a number: '
+  option = gets.chomp.to_i
+  while option.nil? || option < 1 || option > 7
+    puts 'Please enter a number between 1 and 7!'
+    print 'Enter a number: '
+    option = gets.chomp.to_i
+  end
+  option
+end
 
+def option_methods(option)
+  # app = App.new
+  case option
+  when 1
+    display_all_books
+  when 2
+    display_all_people
+  when 3
+    create_person
+  when 4
+    create_book
+  when 5
+    create_rental
+  when 6
+    list_rental_by_id
+  else
+    puts 'Thank you for using this app!'
+    exit
+  end
+end
 
 def main()
   app = App.new
