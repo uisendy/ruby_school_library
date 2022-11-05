94a69a9f300a9ee5a7ba5819127f976397e3282e
Inieke Sendy <ini.e.sendy@gmail.com>
2022-11-05 14:34:34 +0100

    [UI]: Add: Methods: Query: Rentals by id

--- app.rb
+++ app.rb
@@ -82,6 +82,13 @@ class App
     puts 'Rental created successfully'
   end
 
+  def list_rental_by_id
+    print 'Id of person: '
+    id = gets.chomp.to_i
+    @rentals.each do |rental|
+      puts "Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
+    end
+  end
 
   def run()
     puts 'run has been called'
