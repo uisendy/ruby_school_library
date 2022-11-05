6bebdf75d47098d368f779e28af07ccd10f2e79f
Inieke Sendy <ini.e.sendy@gmail.com>
2022-11-05 14:34:08 +0100

    [UI]: Add: Methods: Create: rentals

--- app.rb
+++ app.rb
@@ -67,6 +67,22 @@ class App
     puts 'Book created successfully'
   end
 
+  def create_rental
+    puts 'Select a book from the following list by number'
+    display_all_books
+    selected_book_option = gets.chomp.to_i
+    display_all_people
+    selected_person_option = gets.chomp.to_i
+    print 'Date: '
+    date = gets.chomp
+    book = @books[selected_book_option]
+    person = @people[selected_person_option]
+    rental = Rental.new(date, book, person)
+    @rentals << rental
+    puts 'Rental created successfully'
+  end
+
+
   def run()
     puts 'run has been called'
     prompt
