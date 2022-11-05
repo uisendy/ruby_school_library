58230ba40fc9ede2d05ec1100640812c06049843
Inieke Sendy <ini.e.sendy@gmail.com>
2022-11-05 14:33:10 +0100

    [UI]: Add: Methods: Create: Students and Teachers

--- app.rb
+++ app.rb
@@ -25,7 +25,40 @@ class App
     end
   end
 
+  def create_person
+    print 'Do you want to create a student(1) or a teacher(2)? [Input the number]: '
+    person_option = gets.chomp.to_i
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    if person_option == 1
+      print 'Classroom: '
+      classroom_ = gets.chomp
+      print 'Has parent permmission? [Y/N]: '
+      parent_permmission = gets.chomp.downcase == 'y'
+      create_student(name, age, parent_permmission, classroom_)
+    else
+      print 'Specialization: '
+      specialization = gets.chomp
+      create_teacher(name, age, specialization)
+    end
+    puts 'Person created successfully'
+  end
+
+  def create_student(name, age, parent_permmission, classroom_)
+    classroom = Classroom.new(classroom_)
+    student = Student.new(age, name, parent_permmission)
+    @people << student
+  end
+
+  def create_teacher(name, age, specialization)
+    teacher = Teacher.new(specialization, age, name)
+    @people << teacher
+  end
+
   
+
   def run()
     puts 'run has been called'
     prompt
