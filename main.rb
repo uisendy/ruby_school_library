0bb67150249a3256655cb71be6a3162a3886accc
Inieke Sendy <ini.e.sendy@gmail.com>
2022-11-05 14:37:07 +0100

    [UI]: Add: Methods: display Options

--- main.rb
+++ main.rb
@@ -10,6 +10,17 @@ def prompt
   end
 end
 
+def display_options
+  puts 'Please choose one of the options: '
+  puts '1. - List all books'
+  puts '2. - List all people'
+  puts '3. - Create a person'
+  puts '4. - Create a book'
+  puts '5. - Create a rental'
+  puts '6. - List all rentals for a given person ID'
+  puts '7. - Exit'
+end
+
 
 
 def main()
